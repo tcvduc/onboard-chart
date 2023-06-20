f6d0e391399c618016f325b69cc5739feccce48e
tcvduc <caovanducs@gmail.com>
2023-06-20 08:08:04 +0700

    onboard chart

---
+++ 1/1.js
@@ -0,0 +1,13 @@
+(() => {
+  const classes = {
+    layerChart: "layerChart",
+  };
+
+  function main() {
+    const layerChart = window.document.getElementsByClassName(
+      classes.layerChart
+    )[0];
+  }
+
+  main();
+})();
